020fc30df5708f50ce311807c4ea205b3f7eeae4
kazakhstanmixit-afk <kazakhstanmixit@gmail.com>
2026-09-07 15:41:27 +0500

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { DEFAULT_MODELS, BLOCKS, generateForModels } = require("./openrouter");
 
 const app = express();
 app.use(express.json({ limit: "1mb" }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(__dirname));
 
 // -------- API --------
 
